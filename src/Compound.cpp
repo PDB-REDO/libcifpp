5c3c6fec0938b4de2ba99f98ba3cd1fd5992dd63
Maarten L. Hekkelman <maarten@hekkelman.com>
2021-10-29 10:48:09 +0200

    strip newlines from compound names read from CCD

--- src/Compound.cpp
+++ src/Compound.cpp
@@ -125,6 +125,9 @@ Compound::Compound(cif::Datablock &db)
 	cif::tie(mID, mName, mType, mFormula, mFormulaWeight, mFormalCharge) =
 		chemComp.front().get("id", "name", "type", "formula", "formula_weight", "pdbx_formal_charge");
 
+	// The name should not contain newline characters since that triggers validation errors later on
+	ba::replace_all(mName, "\n", "");
+
 	auto &chemCompAtom = db["chem_comp_atom"];
 	for (auto row : chemCompAtom)
 	{
