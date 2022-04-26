af125bdd5754462040320ed7f52ba6795b2d1c88
Maarten L. Hekkelman <maarten@hekkelman.com>
2022-04-26 16:04:13 +0200

    backup

--- include/cif++/Structure.hpp
+++ include/cif++/Structure.hpp
@@ -214,6 +214,10 @@ class Atom
 	std::string pdbID() const;   // auth_comp_id + '_' + auth_asym_id + '_' + auth_seq_id + pdbx_PDB_ins_code
 
 	bool operator==(const Atom &rhs) const;
+	bool operator!=(const Atom &rhs) const
+	{
+		return not operator==(rhs);
+	}
 
 	// access data in compound for this atom
 
@@ -237,6 +241,13 @@ class Atom
 	}
 
 	friend std::ostream &operator<<(std::ostream &os, const Atom &atom);
+
+	/// \brief Synchronize data with underlying cif data
+	void sync()
+	{
+		if (mImpl)
+			mImpl->prefetch();
+	}
 
   private:
 	friend class Structure;
@@ -289,6 +300,13 @@ typedef std::vector<Atom> AtomView;
 
 // --------------------------------------------------------------------
 
+enum class EntityType
+{
+	Polymer, NonPolymer, Macrolide, Water, Branched
+};
+
+// --------------------------------------------------------------------
+
 class Residue
 {
   public:
@@ -316,10 +334,7 @@ class Residue
 	AtomView &atoms();
 	const AtomView &atoms() const;
 
-	void addAtom(const Atom &atom)
-	{
-		mAtoms.push_back(atom);
-	}
+	void addAtom(Atom &atom);
 
 	/// \brief Unique atoms returns only the atoms without alternates and the first of each alternate atom id.
 	AtomView unique_atoms() const;
@@ -335,6 +350,8 @@ class Residue
 	const std::string &asymID() const { return mAsymID; }
 	int seqID() const { return mSeqID; }
 	std::string entityID() const;
+
+	EntityType entityType() const;
 
 	std::string authAsymID() const;
 	std::string authSeqID() const;
@@ -372,6 +389,11 @@ class Residue
 	friend std::ostream &operator<<(std::ostream &os, const Residue &res);
 
 	friend Structure;
+
+	bool operator==(const Residue &rhs) const
+	{
+		return mStructure == rhs.mStructure and mCompoundID == rhs.mCompoundID and mAsymID == rhs.mAsymID and mSeqID == rhs.mSeqID and mAuthSeqID == rhs.mAuthSeqID;
+	}
 
   protected:
 	Residue() {}
@@ -447,6 +469,11 @@ class Monomer : public Residue
 	// for LEU and VAL
 	float chiralVolume() const;
 
+	bool operator==(const Monomer &rhs) const
+	{
+		return mPolymer == rhs.mPolymer and mIndex == rhs.mIndex;
+	}
+
   private:
 	const Polymer *mPolymer;
 	size_t mIndex;
@@ -498,16 +525,22 @@ class Sugar : public Residue
 	int num() const { return std::stoi(mAuthSeqID); }
 	std::string name() const;
 
-	// Sugar &
+	/// \brief Return the atom the C1 is linked to
+	Atom getLink() const { return mLink; }
+	void setLink(Atom link) { mLink = link; }
 
   private:
+
 	const Branch &mBranch;
+	Atom mLink;
 };
 
 class Branch : public std::vector<Sugar>
 {
   public:
 	Branch(Structure &structure, const std::string &asymID);
+
+	void linkAtoms();
 
 	std::string name() const;
 	float weight() const;
@@ -521,6 +554,8 @@ class Branch : public std::vector<Sugar>
 
   private:
 	friend Sugar;
+
+	std::string name(const Sugar &s) const;
 
 	Structure *mStructure;
 	std::string mAsymID;
@@ -591,6 +626,9 @@ class Structure
 
 	const AtomView &atoms() const { return mAtoms; }
 	// AtomView &atoms() { return mAtoms; }
+
+	EntityType getEntityTypeForEntityID(const std::string entityID) const;
+	EntityType getEntityTypeForAsymID(const std::string asymID) const;
 
 	AtomView waters() const;
 
@@ -673,6 +711,18 @@ class Structure
 	void changeResidue(Residue &res, const std::string &newCompound,
 		const std::vector<std::tuple<std::string, std::string>> &remappedAtoms);
 
+	/// \brief Remove a residue, can be monomer or nonpoly
+	///
+	/// \param asym_id     The asym ID
+	/// \param seq_id      The sequence ID
+	void removeResidue(const std::string &asym_id, int seq_id, const std::string &auth_seq_id)
+	{
+		removeResidue(getResidue(asym_id, seq_id, auth_seq_id));
+	}
+
+	/// \brief Remove residue \a res, can be monomer or nonpoly
+	void removeResidue(Residue &res);
+	
 	/// \brief Create a new non-polymer entity, returns new ID
 	/// \param mon_id	The mon_id for the new nonpoly, must be an existing and known compound from CCD
 	/// \return			The ID of the created entity
@@ -694,17 +744,20 @@ class Structure
 	/// \return				The newly create asym ID
 	std::string createNonpoly(const std::string &entity_id, std::vector<std::vector<cif::Item>> &atom_info);
 
+	/// \brief Create a new (sugar) branch with one first NAG containing atoms constructed from \a nag_atom_info
+	Branch &createBranch(std::vector<std::vector<cif::Item>> &nag_atom_info);
+
 	/// \brief Create a new (sugar) branch with one first NAG containing atoms \a nag_atoms
-	Branch &createBranch(std::vector<std::vector<cif::Item>> &nag_atoms);
+	Branch &createBranch(std::vector<Atom> &nag_atoms);
+
+	/// \brief Extend an existing (sugar) branch identified by \a asymID with one sugar containing atoms constructed from \a atom_info
+	Branch &extendBranch(const std::string &asym_id, std::vector<std::vector<cif::Item>> &atom_info);
 
 	/// \brief Extend an existing (sugar) branch identified by \a asymID with one sugar containing atoms \a atoms
-	Branch &extendBranch(const std::string &asym_id, std::vector<std::vector<cif::Item>> &atoms);
+	Branch &extendBranch(const std::string &asym_id, std::vector<Atom> &atoms);
 
-	/// \brief Remove a residue, can be monomer or nonpoly
-	///
-	/// \param asym_id     The asym ID
-	/// \param seq_id      The sequence ID
-	void removeResidue(const std::string &asym_id, int seq_id);
+	/// \brief Remove \a branch
+	void removeBranch(Branch &branch);
 
 	/// \brief Translate the coordinates of all atoms in the structure by \a t
 	void translate(Point t);
---
+++ test/sugar-test.cpp
@@ -0,0 +1,149 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ * 
+ * Copyright (c) 2021 NKI/AVL, Netherlands Cancer Institute
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define BOOST_TEST_ALTERNATIVE_INIT_API
+#include <boost/test/included/unit_test.hpp>
+
+#include <stdexcept>
+
+#include "cif++/Cif++.hpp"
+#include "cif++/Structure.hpp"
+
+// --------------------------------------------------------------------
+
+cif::File operator""_cf(const char* text, size_t length)
+{
+    struct membuf : public std::streambuf
+    {
+        membuf(char* text, size_t length)
+        {
+            this->setg(text, text, text + length);
+        }
+    } buffer(const_cast<char*>(text), length);
+
+    std::istream is(&buffer);
+    return cif::File(is);
+}
+
+// --------------------------------------------------------------------
+
+std::filesystem::path gTestDir = std::filesystem::current_path();
+
+bool init_unit_test()
+{
+    cif::VERBOSE = 1;
+
+	// not a test, just initialize test dir
+	if (boost::unit_test::framework::master_test_suite().argc == 2)
+		gTestDir = boost::unit_test::framework::master_test_suite().argv[1];
+
+	// do this now, avoids the need for installing
+	cif::addFileResource("mmcif_pdbx_v50.dic", gTestDir / ".." / "rsrc" / "mmcif_pdbx_v50.dic");
+
+	// initialize CCD location
+	cif::addFileResource("components.cif", gTestDir / ".." / "data" / "ccd-subset.cif");
+
+	mmcif::CompoundFactory::instance().pushDictionary(gTestDir / "HEM.cif");
+
+	return true;
+}
+
+// --------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(sugar_name_1)
+{
+	using namespace cif::literals;
+
+	const std::filesystem::path example(gTestDir / "1juh.cif.gz");
+	mmcif::File file(example.string());
+	mmcif::Structure s(file);
+
+	auto &db = s.datablock();
+	auto &entity = db["entity"];
+
+	auto &branches = s.branches();
+
+	BOOST_CHECK_EQUAL(branches.size(), 4);
+
+	for (auto &branch : branches)
+	{
+		auto entityID = branch.front().entityID();
+
+		auto name = entity.find1<std::string>("id"_key == entityID, "pdbx_description");
+		BOOST_CHECK_EQUAL(branch.name(), name);
+	}
+}
+
+// --------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(create_sugar_1)
+{
+	using namespace cif::literals;
+
+	const std::filesystem::path example(gTestDir / "1juh.cif.gz");
+	mmcif::File file(example.string());
+	mmcif::Structure s(file);
+
+	// collect atoms from asym L first
+	auto &NAG = s.getResidue("L");
+	auto nagAtoms = NAG.atoms();
+
+	s.removeResidue(NAG);
+
+	auto &branch = s.createBranch(nagAtoms);
+
+	BOOST_CHECK_EQUAL(branch.name(), "2-acetamido-2-deoxy-beta-D-glucopyranose");
+	BOOST_CHECK_EQUAL(branch.size(), 1);
+}
+
+// --------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(create_sugar_2)
+{
+	using namespace cif::literals;
+
+	const std::filesystem::path example(gTestDir / "1juh.cif.gz");
+	mmcif::File file(example.string());
+	mmcif::Structure s(file);
+
+	// Get branch for H
+	auto &bH = s.getBranchByAsymID("H");
+
+	BOOST_CHECK_EQUAL(bH.size(), 2);
+
+	auto a_sH1 = bH[0].atoms();
+	auto a_sH2 = bH[1].atoms();
+
+	s.removeBranch(bH);
+
+	auto &bN = s.createBranch(a_sH1);
+	s.extendBranch(bN.asymID(), a_sH2);
+
+	BOOST_CHECK_EQUAL(bN.name(), "2-acetamido-2-deoxy-beta-D-glucopyranose-(1-4)-2-acetamido-2-deoxy-beta-D-glucopyranose");
+	BOOST_CHECK_EQUAL(bN.size(), 2);
+
+	file.save(gTestDir / "test-create_sugar_2.cif");
+}
