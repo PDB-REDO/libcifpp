341b2481753d4e234ab03b3bc5f481a6219785fd
Maarten L. Hekkelman <maarten@hekkelman.com>
2026-02-12 10:58:08 +0100

    There's still a lot of work to do

--- test/model-test.cpp
+++ test/model-test.cpp
@@ -54,7 +54,7 @@ TEST_CASE("create_nonpoly_1")
 
 	cif::file file;
 	auto &&[dbi, ignore] = file.emplace("TEST"); // create a datablock
-	dbi->set_validator(&cif::validator_factory::instance().get("mmcif_pdbx.dic"));
+	dbi->set_validator(cif::validator_factory::instance().get("mmcif_pdbx.dic"));
 
 	cif::mm::structure structure(file);
 
@@ -82,7 +82,7 @@ _atom_site.pdbx_formal_charge
 # that's enough to test with
 )"_cf;
 
-	atoms.front().set_validator(&cif::validator_factory::instance().get("mmcif_pdbx.dic"));
+	atoms.front().set_validator(cif::validator_factory::instance().get("mmcif_pdbx.dic"));
 
 	auto &hem_data = atoms["HEM"];
 	auto &atom_site = hem_data["atom_site"];
@@ -159,7 +159,7 @@ _struct_asym.details                       ?
 _atom_type.symbol   C
 )"_cf;
 
-	expected.front().set_validator(&cif::validator_factory::instance().get("mmcif_pdbx.dic"));
+	expected.front().set_validator(cif::validator_factory::instance().get("mmcif_pdbx.dic"));
 
 	if (not(expected.front() == structure.get_datablock()))
 	{
@@ -178,7 +178,7 @@ TEST_CASE("create_nonpoly_2")
 
 	cif::file file;
 	auto &&[dbi, ignore] = file.emplace("TEST"); // create a datablock
-	dbi->set_validator(&cif::validator_factory::instance().get("mmcif_pdbx.dic"));
+	dbi->set_validator(cif::validator_factory::instance().get("mmcif_pdbx.dic"));
 
 	cif::mm::structure structure(file);
 
@@ -270,7 +270,7 @@ _struct_asym.details                       ?
 _atom_type.symbol   C
 )"_cf;
 
-	expected.front().set_validator(&cif::validator_factory::instance().get("mmcif_pdbx.dic"));
+	expected.front().set_validator(cif::validator_factory::instance().get("mmcif_pdbx.dic"));
 
 	REQUIRE(expected.front() == structure.get_datablock());
 
@@ -354,7 +354,7 @@ _struct_asym.details                       ?
 #
 )"_cf;
 
-	data.front().set_validator(&cif::validator_factory::instance().get("mmcif_pdbx.dic"));
+	data.front().set_validator(cif::validator_factory::instance().get("mmcif_pdbx.dic"));
 
 	cif::mm::structure s(data);
 
@@ -566,7 +566,7 @@ _struct_asym.details                       ?
 #
 )"_cf;
 
-	data.front().set_validator(&cif::validator_factory::instance().get("mmcif_pdbx.dic"));
+	data.front().set_validator(cif::validator_factory::instance().get("mmcif_pdbx.dic"));
 
 	SECTION("max")
 	{
