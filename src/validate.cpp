3d29c0ac1ba903bb3d4ae74bc190356c9217f45d
Maarten L. Hekkelman <maarten@hekkelman.com>
2026-08-05 10:17:44 +0200

    Better check_version

--- src/validate.cpp
+++ src/validate.cpp
@@ -631,14 +631,14 @@ bool validator_factory::check_version(std::string_view name, std::string_view ex
 	while (eli != el.end() and fli != fl.end())
 	{
 		int e_int, f_int;
-		if (auto [ptr, ec] = std::from_chars(eli->data(), eli->data() + eli->length(), e_int); ec != std::errc{})
+		if (auto [ptr, ec] = std::from_chars(eli->data(), eli->data() + eli->length(), e_int); ec != std::errc{} and ptr == eli->data() + eli->length())
 		{
 			std::clog << "Could not parse requested version string for dictionary " << std::quoted(expected) << "\n";
 			result = false;
 			break;
 		}
 
-		if (auto [ptr, ec] = std::from_chars(fli->data(), fli->data() + fli->length(), f_int); ec != std::errc{})
+		if (auto [ptr, ec] = std::from_chars(fli->data(), fli->data() + fli->length(), f_int); ec != std::errc{} and ptr == fli->data() + fli->length())
 		{
 			std::clog << "Could not parse version string in dictionary " << name << " " << std::quoted(found) << "\n";
 			result = false;
